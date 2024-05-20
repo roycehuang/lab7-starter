a79e401ad073d430556a55ee02426d1aadb92c62
Joseph Jun <54129361+j2jun@users.noreply.github.com>
2024-05-20 08:34:54 -0700

    Completed Part 1 - Expose

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -8,10 +8,15 @@ class RecipeCard extends HTMLElement {
 
 		// EXPOSE - START (All expose numbers start with A)
 		// A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
+		this.attachShadow({mode: 'open'});
 		// A2. TODO - Create an <article> element - This will hold our markup once our data is set
+		this.article = document.createElement('article');
 		// A3. TODO - Create a style element - This will hold all of the styles for the Web Component
+		this.style = document.createElement('style');
 		// A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made (copy everything INSIDE the <style> tag>)
+		this.style.innerHTML = cardTemplate;
 		// A5. TODO - Append the <style> and <article> elements to the Shadow DOM
+		this.shadowRoot.appendChild(this.style, this.article);
 	}
 
 	/**
@@ -40,6 +45,7 @@ class RecipeCard extends HTMLElement {
 		if (!data) return;
 
 		// A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
+		const article = this.article;
 		// A7. TODO - Set the contents of the <article> with the <article> template given in
 		//           cardTemplate.html and the data passed in (You should only have one <article>,
 		//           do not nest an <article> inside another <article>). You should use template
@@ -47,8 +53,17 @@ class RecipeCard extends HTMLElement {
 		// 			 Do NOT include the <article> tags within the innerHTML of the element you create.
 		//           Remember to replace all the placeholders in the template with the data passed in.
 		//           i.e. imgSrc, titleLnk, etc
+		article.innerHTML = `<article>
+		<img src="${data.imgSrc}" alt="${data.imgAlt}"/>
+		<h2><a href="${data.titleLnk}">${data.titleTxt}</a></h2>
+		<p>${data.organization}</p>
+		<p>${data.rating} (${data.numRatings} ratings)</p>
+		<p>${data.lengthTime}</p>
+		<p>${data.ingredients}</p>
+		</article>`;
 	}
 }
 
 // A8. TODO - Define the Class as a customElement so that you can create
 //           'recipe-card' elements
+customElements.define('recipe-card', RecipeCard);
